606f5fad6c0e55877da073c863932561068e49ca
Vaibhav <22838.vaibhav@gmail.com>
2025-05-21 23:02:34 -0400

    check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,16 +20,25 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
-      BufferedInputStream in =
-          new BufferedInputStream(clientSocket.getInputStream());
+
+      // Get Byte input stream aka request
+      BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());
+      // Read first four bytes i.e. message size
       byte[] messageSizeBytes = in.readNBytes(4);
-      int messageSize = ByteBuffer.wrap(messageSizeBytes).getInt();
+       // Read next two bytes i.e. api key
       byte[] apiKey = in.readNBytes(2);
-      byte[] apiVersion = in.readNBytes(2);
+      // fetch correlation id
       int correlationId = ByteBuffer.wrap(in.readNBytes(4)).getInt();
+      // write a message size to response
       clientSocket.getOutputStream().write(messageSizeBytes);
+      // write correlation id to response
       var res = ByteBuffer.allocate(4).putInt(correlationId).array();
+      int apiVersion = ByteBuffer.wrap(in.readNBytes(2)).getInt();
       clientSocket.getOutputStream().write(res);    
+      if (apiVersion > 5 || apiVersion < 0){
+        // write error code to response
+        clientSocket.getOutputStream().write(ByteBuffer.allocate(2).putInt(35).array());
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
